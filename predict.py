3023a08b1fbeace745c345f05abe13617cf5999d
Ritabrata Pal <rito3668@gmail.com>
2025-11-22 10:25:20 +0530

    second commit

---
+++ predict.py
@@ -0,0 +1,19 @@
+import pickle
+import pandas as pd
+with open("model.pkl","rb") as f:
+    model = pickle.load(f)
+
+MODEL_VERSION="1.0.0"
+class_labels = model.classes_.tolist()
+def predict_output(user_input:dict):
+    df = pd.DataFrame([user_input])
+    predicted_class = model.predict(df)[0]
+    probabilities = model.predict_proba(df)[0]
+    confidence = max(probabilities)
+    class_probas = dict(zip(class_labels,map(lambda p:round(p,4),probabilities)))
+
+    return {
+        "predicted_class":predicted_class,
+        "confidence":round(confidence,4),
+        "class_probabilities":class_probas
+    }
